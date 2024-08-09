cd8493e22e8d3c34d78fc6365bd6900b5feb4b02
Pedro Henrique <79018158+nukhes@users.noreply.github.com>
2024-08-09 06:27:07 -0300

    fix: arrumação

--- Program.cs
+++ Program.cs
@@ -1,154 +1,165 @@
 ﻿System.Random randomNum = new System.Random(); // instância do objeto "random" para gerar coordenadas aleatórias
-System.ConsoleKey key; // variavel para capturar input
-bool estadoJogo = true; // esta variavel controla se o jogo esta ativo ou não
-bool venceuJogo = false; // variavel que seta o resultado final
+System.ConsoleKey key; // variável para capturar input
+bool estadoJogo = true; // esta variável controla se o jogo está ativo ou não
+bool venceuJogo = false; // variável que seta o resultado final
 
 int d = 8; // linha e coluna para gerar as matrizes
 string[,] campo = new string[d, d]; // matriz do jogo
 string[,] bombas = new string[d, d]; // matriz das bombas e infos
 
 // posição do cursor do jogador
-int x = 0; 
+int x = 0;
 int y = 0;
-int slotNum = 0;
 
 const int bandeirasInicio = 8; // ALTERAR AQUI INFLUÊNCIA NO NÚMERO DE BOMBA AUTOMATICAMENTE
 int bandeiras = bandeirasInicio;
 
 int CalcularNumeroDoSlot(int cordY, int cordX)
 {
-    int slotNum = 0;
-    int d = bombas.GetLength(0);
+	int slotNum = 0;
+	int d = bombas.GetLength(0);
 
-    bool IsWithinBounds(int x, int y) => x >= 0 && x < d && y >= 0 && y < d;
+	bool IsWithinBounds(int x, int y) => x >= 0 && x < d && y >= 0 && y < d;
 
-    void CheckBomb(int x, int y)
-    {
-        if (IsWithinBounds(x, y) && bombas[y, x] == "x")
-        {
-            slotNum++;
-        }
-    }
+	void CheckBomb(int x, int y)
+	{
+		if (IsWithinBounds(x, y) && bombas[y, x] == "x")
+		{
+			slotNum++;
+		}
+	}
 
-    CheckBomb(cordX + 1, cordY); // +1x
-    CheckBomb(cordX + 1, cordY + 1);
-    CheckBomb(cordX + 1, cordY - 1);
+	CheckBomb(cordX + 1, cordY); // +1x
+	CheckBomb(cordX + 1, cordY + 1);
+	CheckBomb(cordX + 1, cordY - 1);
 
-    CheckBomb(cordX - 1, cordY); // -1x
-    CheckBomb(cordX - 1, cordY + 1);
-    CheckBomb(cordX - 1, cordY - 1);
+	CheckBomb(cordX - 1, cordY); // -1x
+	CheckBomb(cordX - 1, cordY + 1);
+	CheckBomb(cordX - 1, cordY - 1);
 
-    CheckBomb(cordX, cordY + 1);
-    CheckBomb(cordX, cordY - 1);
+	CheckBomb(cordX, cordY + 1);
+	CheckBomb(cordX, cordY - 1);
 
-    if (cordY == 0 || cordY == d - 1 || cordX == 0 || cordX == d - 1)
-    {
-        CheckBomb(cordX + 1, cordY);
-        CheckBomb(cordX + 1, cordY + 1);
-        CheckBomb(cordX + 1, cordY - 1);
-        CheckBomb(cordX - 1, cordY);
-        CheckBomb(cordX - 1, cordY + 1);
-        CheckBomb(cordX - 1, cordY - 1);
-        CheckBomb(cordX, cordY + 1);
-        CheckBomb(cordX, cordY - 1);
-    }
+	if (cordY == 0 || cordY == d - 1 || cordX == 0 || cordX == d - 1)
+	{
+		CheckBomb(cordX + 1, cordY);
+		CheckBomb(cordX + 1, cordY + 1);
+		CheckBomb(cordX + 1, cordY - 1);
+		CheckBomb(cordX - 1, cordY);
+		CheckBomb(cordX - 1, cordY + 1);
+		CheckBomb(cordX - 1, cordY - 1);
+		CheckBomb(cordX, cordY + 1);
+		CheckBomb(cordX, cordY - 1);
+	}
 
-    return slotNum;
+	return slotNum;
 }
+
 void CavarBuraco(int cordY, int cordX)
 {
-	bool continuidade = true;
-	int c = 1;
-	if (campo[cordY, cordX] == "o") // caso seja um buraco >CONDIÇÃO UNIVERSAL<
+	if (campo[cordY, cordX] == "o") // caso seja um buraco > CONDIÇÃO UNIVERSAL <
 	{
-		if (bombas[cordY,cordX] == "x") // se cavar a bomba
+		if (bombas[cordY, cordX] == "x") // se cavar a bomba
 		{
 			estadoJogo = false;
 			venceuJogo = false;
-		} else if (CalcularNumeroDoSlot(cordY, cordX) > 0) // se tiver uma bomba ao redor
+		}
+		else if (CalcularNumeroDoSlot(cordY, cordX) > 0) // se tiver uma bomba ao redor
 		{
 			campo[cordY, cordX] = $"{CalcularNumeroDoSlot(cordY, cordX)}";
-		} else
+		}
+		else
 		{
 			campo[cordY, cordX] = $" ";
 
 			switch (cordY)
 			{
 				case 0:
-                    campo[cordY + 1, cordX] = $"{CalcularNumeroDoSlot(cordY + 1, cordX)}";
-                    break;
-                case 7:
-                    campo[cordY - 1, cordX] = $"{CalcularNumeroDoSlot(cordY - 1, cordX)}";
-                    break;
+					campo[cordY + 1, cordX] = $"{CalcularNumeroDoSlot(cordY + 1, cordX)}";
+					break;
+				case 7:
+					campo[cordY - 1, cordX] = $"{CalcularNumeroDoSlot(cordY - 1, cordX)}";
+					break;
 				default:
-                    campo[cordY - 1, cordX] = $"{CalcularNumeroDoSlot(cordY - 1, cordX)}";
-                    campo[cordY + 1, cordX] = $"{CalcularNumeroDoSlot(cordY + 1, cordX)}";
-                    break;
-            }
+					campo[cordY - 1, cordX] = $"{CalcularNumeroDoSlot(cordY - 1, cordX)}";
+					campo[cordY + 1, cordX] = $"{CalcularNumeroDoSlot(cordY + 1, cordX)}";
+					break;
+			}
 
-            switch (cordX)
-            {
-                case 0:
-                    campo[cordY, cordX + 1] = $"{CalcularNumeroDoSlot(cordY, cordX + 1)}";
-                    break;
-                case 7:
-                    campo[cordY, cordX - 1] = $"{CalcularNumeroDoSlot(cordY, cordX - 1)}";
-                    break;
-                default:
-                    campo[cordY, cordX - 1] = $"{CalcularNumeroDoSlot(cordY, cordX - 1)}";
-                    campo[cordY, cordX + 1] = $"{CalcularNumeroDoSlot(cordY, cordX + 1)}";
-                    break;
-            }
-        }
+			switch (cordX)
+			{
+				case 0:
+					campo[cordY, cordX + 1] = $"{CalcularNumeroDoSlot(cordY, cordX + 1)}";
+					break;
+				case 7:
+					campo[cordY, cordX - 1] = $"{CalcularNumeroDoSlot(cordY, cordX - 1)}";
+					break;
+				default:
+					campo[cordY, cordX - 1] = $"{CalcularNumeroDoSlot(cordY, cordX - 1)}";
+					campo[cordY, cordX + 1] = $"{CalcularNumeroDoSlot(cordY, cordX + 1)}";
+					break;
+			}
+		}
 	}
 }
+
 void InputReagir() // identificar os inputs ao longo do jogo
 {
 	key = Console.ReadKey().Key;
-	switch (key) 
+	switch (key)
 	{
 		case ConsoleKey.UpArrow:
-			if (y > 0) {
+			if (y > 0)
+			{
 				y--;
 			}
 			break;
 		case ConsoleKey.DownArrow:
-			if (y < 7) {
+			if (y < 7)
+			{
 				y++;
 			}
 			break;
 		case ConsoleKey.RightArrow:
-			if (x < 7) {
+			if (x < 7)
+			{
 				x++;
 			}
 			break;
 		case ConsoleKey.LeftArrow:
-			if (x > 0) {
+			if (x > 0)
+			{
 				x--;
 			}
 			break;
 		case ConsoleKey.Enter:
-			if (bandeiras > 0 && campo[y, x] == "o") {
+			if (bandeiras > 0 && campo[y, x] == "o")
+			{
 				campo[y, x] = "!";
 				bandeiras--;
-			} else if (campo[y, x] == "!") {
+			}
+			else if (campo[y, x] == "!")
+			{
 				bandeiras++;
 				campo[y, x] = "o";
 			}
 			break;
-        case ConsoleKey.Spacebar:
+		case ConsoleKey.Spacebar:
 			CavarBuraco(y, x);
 			break;
-        case ConsoleKey.R: // o "R" para o jogo
+		case ConsoleKey.R: // o "R" para o jogo
 			estadoJogo = false;
 			break;
 	}
 }
+
 void JogoRefresh() // reseta o jogo
 {
 	// função que limpa as matrizez
-	for (int i = 0; i < d; i++) {
-		for (int ii = 0; ii < d; ii++) {
+	for (int i = 0; i < d; i++)
+	{
+		for (int ii = 0; ii < d; ii++)
+		{
 			campo[i, ii] = "o";
 			bombas[i, ii] = "";
 		}
@@ -158,26 +169,32 @@ void JogoRefresh() // reseta o jogo
 	int bii = 0;
 
 	// inserção aleatória das bombas de acordo com o número de bandeiras setadas
-	for (int ii = 0; ii < bandeiras; ii++) {
-		bi = randomNum.Next(0,7);
-		bii = randomNum.Next(0,7);
-		
-		while (bombas[bi,bii] == "x") { // caso a coordenada gerada de pobrema
-			bi = randomNum.Next(0,7);
-			bii = randomNum.Next(0,7);
+	for (int ii = 0; ii < bandeiras; ii++)
+	{
+		bi = randomNum.Next(0, 7);
+		bii = randomNum.Next(0, 7);
+
+		while (bombas[bi, bii] == "x") // caso a coordenada gerada de pobrema
+		{
+			bi = randomNum.Next(0, 7);
+			bii = randomNum.Next(0, 7);
 		}
-		bombas[bi,bii] = "x";
+		bombas[bi, bii] = "x";
 	}
 
-	// analise das celulas para designar o numero
-	for (int i = 0; i < d; i++) {
-		for (int ii = 0; ii < d; ii++) {
-			if (bombas[i, ii] == "") {
+	// análise das células para designar o número
+	for (int i = 0; i < d; i++)
+	{
+		for (int ii = 0; ii < d; ii++)
+		{
+			if (bombas[i, ii] == "")
+			{
 				bombas[i, ii] = $"{CalcularNumeroDoSlot(i, ii)}";
 			}
 		}
 	}
 }
+
 void DesenharCampo() // desenha o campo no console
 {
 	int c = 0;
@@ -185,33 +202,42 @@ void DesenharCampo() // desenha o campo no console
 	Console.WriteLine("Bandeiras Restantes: {0, 17}", "0" + bandeiras);
 	Console.WriteLine($"\n\nX:{x} Y:{y}");
 
-	for (int i = 0; i < d; i++) {
-		for (int ii = 0; ii < d; ii++) {
+	for (int i = 0; i < d; i++)
+	{
+		for (int ii = 0; ii < d; ii++)
+		{
 			if (campo[i, ii] == "0")
 			{
 				campo[i, ii] = " ";
-            }
-			if (i == y && ii ==x) {
+			}
+			if (i == y && ii == x)
+			{
 				Console.Write($"#{campo[i, ii]}#");
-			} else {
+			}
+			else
+			{
 				Console.Write($"[{campo[i, ii]}]");
 			}
-			//CavarBuraco(i, ii);
 			c++;
 		}
 		Console.WriteLine();
 	}
 }
-void VerificarEstado() // este metodo oira verificar por perdas ou vitórias
+
+void VerificarEstado() // este método ira verificar por perdas ou vitórias
 {
 	int bandeirasEmBomba = 0;
-	for (int i = 0; i < d; i++) {
-		for (int ii = 0; ii < d; ii++) {
-			if (bombas[i, ii] == "x" && campo[i, ii] == "!") {
+	for (int i = 0; i < d; i++)
+	{
+		for (int ii = 0; ii < d; ii++)
+		{
+			if (bombas[i, ii] == "x" && campo[i, ii] == "!")
+			{
 				bandeirasEmBomba++;
 			}
 		}
-		if (bandeirasEmBomba == bandeirasInicio) {
+		if (bandeirasEmBomba == bandeirasInicio)
+		{
 			venceuJogo = true;
 			estadoJogo = false;
 		}
